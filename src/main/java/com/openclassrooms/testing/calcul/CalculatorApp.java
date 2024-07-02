91a42faa50cd12a6da5912f74dd83f8630f2d360
vocaltech <it@vocaltech.fr>
2024-07-02 23:27:11 +0200

    Remove blank line

--- src/main/java/com/openclassrooms/testing/calcul/CalculatorApp.java
+++ src/main/java/com/openclassrooms/testing/calcul/CalculatorApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApp {
-
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApp.class, args);
     }
